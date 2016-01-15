b78e575a790728d0e58ed25b4c0edd32116bdd9d
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-15 15:45:30 -0800

    [feat] create redux root reducer file

--- app/src/redux/rootReducer.js
+++ app/src/redux/rootReducer.js
@@ -1,1 +1,9 @@
+import { combineReducers } from 'redux';
+import { routeReducer as router } from 'redux-simple-router';
+import counter from './modules/counter';
 
+
+export default combineReducers({
+  counter,
+  router
+});
