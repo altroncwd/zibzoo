6ea2b4352547bf25428e7174b57282ba605c76c8
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-15 15:45:57 -0800

    [feat] create counterview

---
+++ app/src/views/components/counterView.jsx
@@ -0,0 +1,29 @@
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { actions as counterActions } from '../../redux/modules/counter';
+
+const mapStateToProps = (state) => ({
+  counter: state.counter
+});
+
+export class CounterView extends React.Component {
+  static propTypes = {
+    counter: PropTypes.any.isRequired,
+    increment: PropTypes.func.isRequired,
+    decrement: PropTypes.func.isRequired
+  };
+
+  render() {
+    const handleIncrement = () => this.props.increment();
+    const handleDecrement = () => this.props.decrement();
+    return (
+      <div>
+        <h1>{this.props.counter}</h1>
+        <button onClick={handleIncrement}>increment</button>
+        <button onClick={handleDecrement}>decrement</button>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, counterActions)(CounterView);
