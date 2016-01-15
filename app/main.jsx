275da36a55f1e3f4ca52ed199facc0ac5dead14e
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-15 15:46:39 -0800

    [feat] mount root app component to DOM

--- app/main.jsx
+++ app/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/views/components/App';
+import App from './src/views/containers/App';
+import configureStore from './src/redux/configureStore';
 
 // comment the following two lines out for deployment
+const store = configureStore(window.__INITIAL_STATE__);
+
 const app = document.createElement('div');
 document.body.appendChild(app);
-ReactDOM.render(<App />, app);
+ReactDOM.render(<App store={store} />, app);
