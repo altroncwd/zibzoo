3e8975c49dd9df78c83a84b03080284334ae5a9e
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-15 19:50:54 -0800

    [rafactor] add routing componentto App

--- app/src/views/containers/App.jsx
+++ app/src/views/containers/App.jsx
@@ -1,17 +1,21 @@
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
-import CounterView from '../components/counterView';
+import { Router } from 'react-router';
 import DevTools from './devTools/devTools';
 
 export default class App extends React.Component {
   static propTypes = {
-    store: PropTypes.object.isRequired
+    store: PropTypes.object.isRequired,
+    routes: PropTypes.element.isRequired,
+    history: PropTypes.object.isRequired
   };
   render() {
     return (
       <Provider store={this.props.store}>
         <div>
-          <CounterView />
+          <Router history={this.props.history}>
+            {this.props.routes}
+          </Router>
           <DevTools/>
         </div>
       </Provider>
