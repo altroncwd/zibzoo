7cb0ea26cc3bf6ef0ae40526bfbd4dda7aebc1ff
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-15 20:07:46 -0800

    [feat] add path to demo routes

--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, IndexRoute, Redirect } from 'react-router';
 
 import CounterView from './src/views/components/counterView';
+import HelloRoutes from './src/views/components/HelloRoutes';
 import CoreLayout from './src/views/core-layout/coreLayout';
 
 export default (
   <Route path="/" component= { CoreLayout } >
     <IndexRoute component={ CounterView } />
+    <Route path="/hello" component={ HelloRoutes } />
     <Redirect from="*" to="/" />
 
   </Route>
