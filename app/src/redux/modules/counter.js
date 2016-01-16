ffc932c086bb385a777c1e30173254f326450b4e
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-15 16:07:51 -0800

    [refactor] fix line length in comment

--- app/src/redux/modules/counter.js
+++ app/src/redux/modules/counter.js
@@ -10,7 +10,8 @@ export const DECREMENT = 'DECREMENT';
 // create Actions using createAction
 // These functions will create standard flux actions
 // standard flux actions are simply objects that must contain an action type and a payload
-// the first action creator below will when invoked produce an object like so: { type: "INCREMENT", payload: 1 }
+// the first action creator below will when invoked produce an object
+// like so: { type: "INCREMENT", payload: 1 }
 export const increment = createAction(INCREMENT, (value = 1) => value);
 export const decrement = createAction(DECREMENT, (value = 1) => value);
 
