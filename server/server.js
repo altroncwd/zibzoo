35f64c1bf14c1245655c5e46f9411a907460875b
Benji Marinacci <bcmarinacci@icloud.com>
2016-01-16 11:41:19 -0800

    add port variable

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const koa = require('koa');
 const static = require('koa-static');
+const port = process.env.PORT || 3000;
 
 const app = koa();
 
 app.use(static(`${__dirname}/../app/`));
 
-app.listen(3000);
+app.listen(port);
