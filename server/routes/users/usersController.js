aa667fc62b9eedff2dba7ae1b96fa7f623216873
tbergen1 <tom@servant.co>
2016-01-19 11:10:40 -0800

    [feat] add signup function

--- server/routes/users/usersController.js
+++ server/routes/users/usersController.js
@@ -1,3 +1,16 @@
+var usersHelpers = require('./../../db/users/usersHelpers.js')
+
 module.exports = {
 
+  signup: function(req, res) {
+    var user = {username: 'MAKDD1DDAACMCDDDDAD'};
+    usersHelpers.postUser(user)
+      .then(function(result) {
+        console.log(result);
+        // res.status(201).send(result);
+      })
+      .catch(function(error) {
+        res.status(401).send(error);
+      })
+  }
 };
