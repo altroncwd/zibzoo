624e53566f5f801352a2589426abdcebfe35bd7c
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-19 15:15:23 -0800

    [refactor] change name of blockgrid directive

--- client/app/components/blockgrid/block-grid-directive.js
+++
@@ -1,8 +0,0 @@
-angular.module('zibzoo.blockgrid.directive', [])
-  .directive('blockgrid', function () {
-    return {
-      restrict: 'E',
-      templateUrl: 'app/components/blockgrid/_blockgrid.html',
-      require: ['^VendorListController', '^LandingController']
-    };
-  });
