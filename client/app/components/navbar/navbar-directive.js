d3f8453e635448937b5a3d40c24627ba9c7a09e4
Benjamin Marinacci <bcmarinacci@icloud.com>
2016-01-18 22:45:51 -0800

    [fix] global file lint

--- client/app/components/navbar/navbar-directive.js
+++ client/app/components/navbar/navbar-directive.js
@@ -1,9 +1,9 @@
 angular.module('zibzoo.navbar.directive', [])
-  .directive('navbar', function() {
+  .directive('navbar', function () {
     return {
       restrict: 'E',
       templateUrl: 'app/components/navbar/_navbar.html',
-      controller: function($scope, $rootScope) {
+      controller: function ($scope, $rootScope) {
         $rootScope.isNavbarCollapsed = true;
       }
     };
