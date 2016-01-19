00c7340d3de03d0dd697056ae98d939c927467b5
tbergen1 <tom@servant.co>
2016-01-19 11:07:03 -0800

    [refactor] add MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
-
+var mongoose = require('mongoose');
+var USERTEST = require('./routes/users/usersController.js');
 var port = process.env.PORT || 3000;
+mongoose.connect('mongodb://localhost:27017');
+mongoose.connection.once('open', function() {
+  console.log("Connected to mongoDB");
+  USERTEST.signup();
 
+})
 require('./config/middleware.js')(app, express);
 
 app.listen(port);
