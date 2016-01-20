72fdebbd233caa29e852b0e872971335e4f1ffcc
Benjamin Marinacci <bcmarinacci@icloud.com>
2016-01-20 10:04:57 -0800

    [refactor] flatten promise chains

--- server/db/users/usersHelpers.js
+++ server/db/users/usersHelpers.js
@@ -2,54 +2,50 @@ var User = require('./usersModel.js');
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-
-
-
 module.exports = {
 
-  postUser: function(userObj) {
-    console.log('USEROBJ', userObj);
-    return User.findOne({
-      'username': userObj.username
-    })
-
-    .then(function(user) {
-      console.log('USER', user);
-      if(user) {
-        throw Error('User already exists!')
-      }
-      var newUser = new User({
+  postUser: function (userObj) {
+    return User
+      .findOne({
         'username': userObj.username
-      });
-      return newUser.save()
-    })
+      })
+      .then(function (user) {
+        if (user) {
+          throw Error('User already exists!')
+        }
+        var newUser = new User({
+          'username': userObj.username
+        });
 
-    .then(function(result) {
-      console.log('RESULT', result.toObject());
-      if(!result) {
-        throw Error('Unable to save user');
-      }
-      return result.toObject();
-    })
-    .catch(function(error) {
-      console.log("Error adding user: ", error);
-    });
+        return newUser.save()
+      })
+      .then(function (result) {
+        if (!result) {
+          throw Error('Unable to save user');
+        }
+
+        return result;
+      })
+      .catch(function (error) {
+        return error;
+      });
   },
 
-  getUser: function(userObj) {
-    User.findOne({
-      'username': userObj.username
-    })
-    .then(function(user) {
-      if(!user) {
-        throw Error('User does not exist');
-      }
-      console.log(user);
-      // res.status(200).send(user);
-    })
-    .catch(function(error) {
-      console.log("Error retrieving user: ", error);
-    })
+  getUser: function (userObj) {
+    return User
+      .findOne({
+        'username': userObj.username
+      })
+      .then(function (user) {
+        if (!user) {
+          throw Error('User does not exist');
+        }
+
+        return user;
+      })
+      .catch(function (error) {
+        return error;
+      });
   }
 
 }
