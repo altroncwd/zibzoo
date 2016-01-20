4e9c79feb5666fbc766f1703d28b43684aa05fa1
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-20 12:00:03 -0800

    [feat] add getVendors func on vendorListController

--- client/app/vendors/VendorsList/vendorsList.js
+++ client/app/vendors/VendorsList/vendorsList.js
@@ -1,4 +1,14 @@
 angular.module('zibzoo.vendors.list', [])
   .controller('VendorsListController', ['$scope', 'vendors', function ($scope, vendors) {
     $scope.vendors = vendors.tempData;
+    $scope.getVendors = function () {
+      vendors.getVendors()
+        .then(function (data) {
+          $scope.vendors = data;
+        })
+        .catch(function (error) {
+          console.error('Error getting Vendors: ', error);
+        });
+    };
+    $scope.getVendors();
   }]);
