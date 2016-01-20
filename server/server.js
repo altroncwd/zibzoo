840420ecb3d8e367500ae5c80a228f15cfc40e4a
Benjamin Marinacci <bcmarinacci@icloud.com>
2016-01-20 15:04:48 -0800

    [cleanup] remove test functions

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var USERTEST = require('./routes/users/usersController.js');
 var port = process.env.PORT || 3000;
-mongoose.connect('mongodb://localhost:27017');
-mongoose.connection.once('open', function() {
-  console.log("Connected to mongoDB");
-  USERTEST.signup();
 
-})
+mongoose.connect('mongodb://localhost:27017');
+
+mongoose.connection.once('open', function () {
+  console.log("Connected to mongoDB.");
+});
+
 require('./config/middleware.js')(app, express);
 
 app.listen(port);
