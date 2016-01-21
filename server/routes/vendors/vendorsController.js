851210f5153d656246a41c7e5d932f28356ce35f
tbergen1 <tom@servant.co>
2016-01-20 19:22:48 -0800

    [feat] add vendor .signUp() and .signIn() request handlers

--- server/routes/vendors/vendorsController.js
+++ server/routes/vendors/vendorsController.js
@@ -1,3 +1,27 @@
+var vendorsHelpers = require('./../../db/vendor/vendorHelpers.js')
+
 module.exports = {
+
+  signUp: function (req, res) {
+    var vendor = req.body;
+    vendorsHelpers.postVendor(vendor)
+      .then(function (result) {
+        res.status(201).send(result);
+      })
+      .catch(function (error) {
+        res.status(401).send(error);
+      });
+  },
+
+  signIn: function (req, res) {
+    var vendor = req.body;
+    vendorsHelpers.getVendor(vendor)
+      .then(function (result) {
+        res.status(200).send(result);
+      })
+      .catch(function (error) {
+        res.status(404).send(error);
+      });
+  }
 
 };
