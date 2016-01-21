bcf8db74f794bd0f20cc21911c51d3c8df52805a
tbergen1 <tom@servant.co>
2016-01-20 19:21:23 -0800

    [feat] add user .signUp() and .signIn() request handlers

--- server/routes/users/usersController.js
+++ server/routes/users/usersController.js
@@ -2,15 +2,26 @@ var usersHelpers = require('./../../db/users/usersHelpers.js')
 
 module.exports = {
 
-  signup: function(req, res) {
-    var user = {username: 'MAKDD1DDAACMCDDDDAD'};
+  signUp: function (req, res) {
+    var user = req.body;
     usersHelpers.postUser(user)
-      .then(function(result) {
-        console.log(result);
-        // res.status(201).send(result);
+      .then(function (result) {
+        res.status(201).send(result);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         res.status(401).send(error);
+      });
+  },
+
+  signIn: function (req, res) {
+    var user = req.body;
+    usersHelpers.getUser(user)
+      .then(function (result) {
+        res.status(200).send(result);
       })
+      .catch(function (error) {
+        res.status(404).send(error);
+      });
   }
+
 };
