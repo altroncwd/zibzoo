ec62f6b8ca3965df90a8d8fe3052e5c485f04365
Christopher Decker <christopher.w.decker@gmail.com>
2016-01-21 12:58:55 -0800

    [refactor] update menu / files to newest versions to solve merge conflicts

--- client/app/merchants/menu/menu-form-directive.js
+++ client/app/merchants/menu/menu-form-directive.js
@@ -1,15 +1,16 @@
 angular.module('zibzoo.menuform.directive', [])
-  .directive('menuForm', function () {
+  .directive('menuForm', function() {
     return {
       restrict: 'E',
-      templateUrl: 'app/merchants/menu/_menu-form.html',
+      templateUrl: 'app/merchants/menu/menu-form.html',
       scope: {
-        menu: '='
+        menu: "="
       },
-      controller: function ($scope) {
-        $scope.addItem = function () {
-          $scope.menu.addItem();
+      controller: function($scope) {
+        $scope.addItem = function(food, price, calories, description, prepTime) {
+          $scope.menu.addItem(food, price, calories, description, prepTime);
         };
+
       }
     };
   });
