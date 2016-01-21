dfdd45cd9214687cbf29e725e02d9ca1110a6565
Ian Bari <ian.bari@att.net>
2016-01-21 10:22:47 -0800

    [feat]: add menu item order form

---
+++ client/app/vendor/order-form.js
@@ -0,0 +1,23 @@
+angular.module('zibzoo.vendor.orderform', [])
+  .controller('OrderFormController', ['$scope', '$modalInstance', 'item', function ($scope, $modalInstance, item) {
+    $scope.item = item;
+    $scope.quantity = 1;
+
+    $scope.addToCart = function () {
+      console.log('Item ordered: ', $scope.item);
+      console.log('Quantity ordered: ', $scope.quantity);
+      $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+
+    $scope.increment = function () {
+      ++$scope.quantity;
+    };
+
+    $scope.decrement = function () {
+      --$scope.quantity; 
+    };
+  }]);
--- client/app/vendor/vendor.js
+++ client/app/vendor/vendor.js
@@ -1,3 +1,29 @@
 angular.module('zibzoo.vendor', [])
-  .controller('VendorController', ['$scope', function ($scope) {
+  .controller('VendorController', ['$scope', '$stateParams', 'vendor', '$modal', function ($scope, $stateParams, vendor, $modal) {
+    $scope.vendor = vendor;
+    $scope.items  = _.chunk($scope.vendor.menu, 2);
+
+    $scope.getVendor = function (params) {
+      vendor.getVendor(params)
+        .then(function (data) {
+          $scope.vendor = data;
+        })
+        .catch(function (error) {
+          console.error('Error getting vendor: ', error);
+        });
+    };
+
+    $scope.open = function (item) {
+      var modalInstance = $modal.open({
+        templateUrl: 'app/vendor/_order-form.html',
+        controller: 'OrderFormController',
+        resolve: {
+          item: function () {
+            return item;
+          }
+        }
+      });
+    };
+
+    //$scope.getVendor($stateParams.vendorId);
   }]);
