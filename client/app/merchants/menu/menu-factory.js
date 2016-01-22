0e2005fc75b235df7b95aef3e5de710b8e454e76
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-22 11:19:56 -0800

    [fix] update vendor name and remove tempData

--- client/app/merchants/menu/menu-factory.js
+++ client/app/merchants/menu/menu-factory.js
@@ -3,24 +3,24 @@ angular.module('zibzoo.merchant.menu.factory', [])
     var menu = {};
 
     menu.items = [
-      { food: 'cheese',
-        price: 14.50,
-        description: 'three cheese pizza with a garlic stuffed crust',
-        inStock: true,
-        prepTime: 20,
-      },
-      { food: 'volcano',
-        price: 18.00,
-        description: 'three cheese topped with jalapenos, red pepers, spicy italian sausage and a stuffed jalapeno creamcheese crust ',
-        inStock: true,
-        prepTime: 22,
-      },
-      { food: 'garlic breadsticks',
-        price: 7.0,
-        description: 'soft homemade breadsticks brushed with butter and our special mix of garlic and spices',
-        inStock: true,
-        prepTime: 15,
-      }
+      // { food: 'cheese',
+      //   price: 14.50,
+      //   description: 'three cheese pizza with a garlic stuffed crust',
+      //   inStock: true,
+      //   prepTime: 20,
+      // },
+      // { food: 'volcano',
+      //   price: 18.00,
+      //   description: 'three cheese topped with jalapenos, red pepers, spicy italian sausage and a stuffed jalapeno creamcheese crust ',
+      //   inStock: true,
+      //   prepTime: 22,
+      // },
+      // { food: 'garlic breadsticks',
+      //   price: 7.0,
+      //   description: 'soft homemade breadsticks brushed with butter and our special mix of garlic and spices',
+      //   inStock: true,
+      //   prepTime: 15,
+      // }
     ];
 
     menu.addItem = function (food, price, calories, description, prepTime) {
