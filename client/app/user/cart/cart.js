e640d4f26b166bf659c997ba30cb4d965371450b
Ian Bari <ian.bari@att.net>
2016-01-21 21:28:33 -0800

    [feat]: users should be able to remove cart items

--- client/app/user/cart/cart.js
+++ client/app/user/cart/cart.js
@@ -1,4 +1,8 @@
 angular.module('zibzoo.cart', [])
   .controller('CartController', ['$scope', 'User', function ($scope, User) {
     $scope.cart = User.data.orders;
+
+    $scope.removeItem = function (index) {
+      $scope.cart.splice(index, 1);
+    };
   }]);
