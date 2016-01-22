478a0c878635d86b3f3ed4a92cf85044ecea64fe
tbergen1 <tom@servant.co>
2016-01-22 14:30:30 -0800

    [refactor] add vendor population

--- spec/server/vendorsHelpers.spec.js
+++ spec/server/vendorsHelpers.spec.js
@@ -1,10 +1,12 @@
 var vendorsHelpers = require('../../server/db/vendors/vendorsHelpers.js');
+var menusHelpers = require('../../server/db/menus/menusHelpers.js');
 var mongoose = require('mongoose');
 
 describe('vendorsHelpers', function () {
   var mockVendor = {
     name: "Willy's Chili"
   };
+  var mockMenu = {};
 
   // Disconnect from database after all tests have run -- NOTE: must be in last file in server/
   afterAll(function () {
@@ -21,7 +23,9 @@ describe('vendorsHelpers', function () {
 
       postPromise
         .then(function (result) {
+          mockMenu.vendorId = result._id;
           expect(result.name).toBe("Willy's Chili");
+
           done();
         });
     });
@@ -38,6 +42,29 @@ describe('vendorsHelpers', function () {
   });
 
   describe('.getVendor()', function () {
+
+    var menuItems = {};
+    menuItems.items = [{ name: 'Chicken' }, { name: 'Beef' }];
+
+    beforeAll(function (done) {
+      var menuPromise = menusHelpers.postMenu(mockMenu);
+
+      menuPromise
+        .then(function (result) {
+          menuItems.menuId = result._id;
+          for (var i = 0; i < menuItems.items.length; i++) {
+            menuItems.items[i].menuId = result._id;
+          }
+
+          menusHelpers.postMenuItems(menuItems)
+            .then(function (data) {
+              console.log('\nMENU ITEMS', data);
+            });
+
+          done();
+        });
+    });
+
     it('should be a function', function () {
       expect(typeof vendorsHelpers.postVendor).toBe('function');
     });
