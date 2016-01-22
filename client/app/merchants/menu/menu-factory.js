e8a353e03bcd63de1e93102c9b4077d842515474
Christopher Decker <christopher.w.decker@gmail.com>
2016-01-21 19:45:22 -0800

    [fix] update menu factory/form for linting

--- client/app/merchants/menu/menu-factory.js
+++ client/app/merchants/menu/menu-factory.js
@@ -1,47 +1,51 @@
 angular.module('zibzoo.merchant.menu.factory', [])
+<<<<<<< 2848f4752342d52ee8fc0007df30a3c4927b3ba3
   .factory('menu', function() {
+=======
+  .factory('menu', function () {
+>>>>>>> [cleanup] lint files
     var menu = {};
 
     menu.items = [
-      { food : "cheese",
-        price : 14.50,
-        description : "three cheese pizza with a garlic stuffed crust",
-        inStock : true,
-        prepTime : 20 ,
+      { food: 'cheese',
+        price: 14.50,
+        description: 'three cheese pizza with a garlic stuffed crust',
+        inStock: true,
+        prepTime: 20,
       },
-      { food : "volcano",
-        price : 18.00,
-        description : "three cheese topped with jalapenos, red pepers, spicy italian sausage and a stuffed jalapeno creamcheese crust ",
-        inStock : true,
-        prepTime : 22 ,
+      { food: 'volcano',
+        price: 18.00,
+        description: 'three cheese topped with jalapenos, red pepers, spicy italian sausage and a stuffed jalapeno creamcheese crust ',
+        inStock: true,
+        prepTime: 22,
       },
-      { food : "garlic breadsticks",
-        price : 7.0,
-        description : "soft homemade breadsticks brushed with butter and our special mix of garlic and spices",
-        inStock : true,
-        prepTime : 15 ,
+      { food: 'garlic breadsticks',
+        price: 7.0,
+        description: 'soft homemade breadsticks brushed with butter and our special mix of garlic and spices',
+        inStock: true,
+        prepTime: 15,
       }
     ];
 
-    menu.addItem = function(food, price, calories, description, prepTime) {
+    menu.addItem = function (food, price, calories, description, prepTime) {
       var item = {
         food: food,
         price: price,
         description: description,
         inStock: true,
-        prepTime: prepTime ,
+        prepTime: prepTime,
       };
       console.log(item);
       menu.items.unshift(item);
     };
-    menu.remove = function (index){
-      var removeFromDb = menu.items.splice(index,1);
-    };
+    // menu.remove = function (index) {
+    //   var removeFromDb = menu.items.splice(index, 1);
+    // };
     // Only for test use
-    menu.log = function(x){
-      console.log("testing");
+    menu.log = function (x) {
+      console.log('testing');
       console.log(x);
-    }
+    };
     // initialize
     // menu.addItem();
 
