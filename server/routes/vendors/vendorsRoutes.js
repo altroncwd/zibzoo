5bcb9d72bec5da92101704917d000582ca857a67
Benji Marinacci <bcmarinacci@icloud.com>
2016-01-23 10:56:39 -0800

    [refactor] change all 'vendors' to 'vendor' for consistency

--- server/routes/vendors/vendorsRoutes.js
+++
@@ -1,9 +0,0 @@
-var vendorsController = require('./vendorsController.js');
-
-module.exports = function (app) {
-
-  app.post('/', vendorsController.signUp);
-  app.get('/', vendorsController.signIn);
-  app.get('/retrieve', vendorsController.retrieveVendor);
-
-};
