e800e874260d2441d109134c3b067b75f440a21f
Christopher Decker <christopher.w.decker@gmail.com>
2016-01-23 12:30:17 -0800

    [feat] continued attempt at testing, commented out for now

--- spec/client/merchantOrder.speck.js
+++ spec/client/merchantOrder.speck.js
@@ -1,26 +1,48 @@
-describe('Merchant order view', function () {
-  var $scope;
-  var $rootScope;
-  var createController;
-  var httpBackend;
+// describe('Merchant order view', function () {
+//   var $scope;
+//   var $rootScope;
+//   var createController;
+//   var httpBackend;
 
-  beforeEach(module('zibzoo'));
-  beforeEach(inject(function ($injector) {
-    $rootScope = $injector.get('$rootScope');
-    $httpBackend = $injector.get('$httpBackend');
+//   beforeEach(module('zibzoo'));
+//   beforeEach(inject(function ($injector) {
+//     $rootScope = $injector.get('$rootScope');
+//     $httpBackend = $injector.get('$httpBackend');
 
-    $scope = $rootScope.$new();
+//     $scope = $rootScope.$new();
 
-    var $controller = $injector.get('$controller');
+//     var $controller = $injector.get('$controller');
 
-    createController = function () {
-      return $controller('MerchantOrdersController', {
-        $scope: $scope
-      });
-    };
+//     createController = function () {
+//       return $controller('MerchantOrdersController', {
+//         // $scope: $scope
+//       });
+//     };
+
+//     // don't know if I need this but i'm sure the routes will need to change even if i do
+//     var fakeVendors = [{}, {}, {}];
+//     $httpBackend.whenGET('app/landing/landing.html').respond(fakeVendors);
+//     $httpBackend.whenGET('api/vendors').respond(fakeVendors);
+//     createController();
+//     $httpBackend.flush();
+//   }));
+
+//   // dont know if i'll need this either
+//   afterEach(function () {
+//     $httpBackend.verifyNoOutstandingExpectation();
+//     $httpBackend.verifyNoOutstandingRequest();
+//   });
+
+//   describe("$scope.ordersList", function () {
+
+//     it('should exist', function () {
+//       console.log($scope);
+//       expect($scope.ordersList).toBeDefined();
+//     });
+//     it('should be an array', function () {
+//       expect($scope.ordersList instanceof Array).toBe(true);
+//     });
+//   });
 
 
-
-  }));
-
-});
+// });
