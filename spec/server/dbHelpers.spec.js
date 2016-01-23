94afe6fd9f07b072c16800a7c41778ea5c6edddf
Tom Bergen <thomasbergen15@gmail.com>
2016-01-22 17:55:10 -0800

    [feat] add getVendor() and postMenuItem()

--- spec/server/dbHelpers.spec.js
+++ spec/server/dbHelpers.spec.js
@@ -5,7 +5,8 @@ var mongoose = require('mongoose');
 // Open a connection to local MongoDB
 mongoose.connect('mongodb://localhost:27017');
 
-describe('Database helper functions', function () {
+
+describe('The database helper function,', function () {
 
   // Drop the database before tests run
   beforeAll(function (done) {
@@ -31,28 +32,32 @@ describe('Database helper functions', function () {
   };
 
   var mockMenuItem = {
-    name: 'Willy Con Carne',
+    name: "Willy's Fajita",
     price: 7,
     inStock: true
   };
 
-  describe('postVendor()', function () {
 
-    it('should be a function', function () {
+  describe('postVendor(),', function () {
+
+    it('should be a function.', function () {
       expect(typeof vendorHelpers.postVendor).toBe('function');
     });
 
-    it('should store a new vendor in the database', function (done) {
+    it('should store a new vendor in the database.', function (done) {
       vendorHelpers.postVendor(mockVendor)
         .then(function (vendor) {
-          // save vendorId in mockMenuItem
+
+          // save vendor _id in mockVendor and mockMenuItem
+          mockVendor._id = vendor._id;
           mockMenuItem.vendorId = vendor._id;
-          expect(vendor.name).toBe("Willy's Chili");
+
+          expect(vendor.name).toBe(mockVendor.name);
           done();
         });
     });
 
-    it('should return an error if a vendor already exists', function (done) {
+    it('should return an error if a vendor already exists.', function (done) {
       vendorHelpers.postVendor(mockVendor)
         .then(function (error) {
           expect(error.message).toBe('Vendor already exists.');
@@ -62,105 +67,59 @@ describe('Database helper functions', function () {
 
   }); // postVendor()
 
-  describe('postMenuItem()', function () {
 
-    it('should be a function', function () {
+  describe('getOneVendor(),', function () {
+
+    it('should be a function.', function () {
       expect(typeof vendorHelpers.postVendor).toBe('function');
     });
 
-    it('should store a new menuItem in the database', function (done) {
+    it('should retrieve an existing vendor from the database.', function (done) {
+      vendorHelpers.getOneVendor(mockVendor)
+        .then(function (vendor) {
+          expect(vendor._id).toEqual(mockVendor._id);
+          expect(vendor.name).toBe(mockVendor.name);
+          done();
+        });
+    });
+
+    it('should return an error if a vendor does not exist.', function (done) {
+      // query the database with a non-existent _id
+      vendorHelpers.getOneVendor({ _id: '56a2c39e63edefd81df25ad2' })
+        .then(function (error) {
+          expect(error.message).toBe('Vendor does not exist.');
+          done();
+        });
+    });
+
+  }); // getOneVendor()
+
+
+  describe('postMenuItem(),', function () {
+
+    it('should be a function.', function () {
+      expect(typeof vendorHelpers.postVendor).toBe('function');
+    });
+
+    it('should store a new menuItem in the database.', function (done) {
       menuItemHelpers.postMenuItem(mockMenuItem)
         .then(function (menuItem) {
+          expect(menuItem._id).toBeDefined();
           expect(menuItem.vendorId).toBe(mockMenuItem.vendorId);
-          expect(menuItem.name).toBe('Willy Con Carne');
+          expect(menuItem.name).toBe(mockMenuItem.name);
+          done();
+        });
+    });
+
+    it('should update the vendor with the new menu item _id.', function (done) {
+      vendorHelpers.getOneVendor(mockVendor)
+        .then(function (vendor) {
+          expect(vendor.menuItems.length).toBe(1);
+          expect(vendor.menuItems[0].name).toEqual(mockMenuItem.name);
           done();
         });
     });
 
   }); // postMenuItem()
 
-  xdescribe('getVendor()', function () {
-
-    it('should be a function', function () {
-      expect(typeof vendorHelpers.postVendor).toBe('function');
-    });
-
-    it('should retrieve an existing vendor from the database', function (done) {
-      var getPromise = vendorHelpers.getVendor(mockVendor);
-
-      getPromise
-        .then(function (vendor) {
-          expect(vendor.name).toBe("Willy's Chili");
-          done();
-        });
-    });
-
-    xit('should return an error if a vendor already exists', function (done) {
-      var getPromise = vendorHelpers.getVendor(mockVendor);
-
-      getPromise
-        .then(function (error) {
-          expect(error.message).toBe('Vendor already exists.');
-          done();
-        });
-    });
-
-  }); // getVendor()
-
-
-
-
-
-
 }); // Database helper functions
-
-  // });
-
-  // describe('.getVendor()', function () {
-
-  //   var menuItems = {};
-  //   menuItems.items = [{ name: 'Chicken' }, { name: 'Beef' }];
-
-  //   beforeAll(function (done) {
-  //     var menuPromise = menusHelpers.postMenu(mockMenu);
-
-  //     menuPromise
-  //       .then(function (result) {
-  //         menuItems.menuId = result._id;
-  //         for (var i = 0; i < menuItems.items.length; i++) {
-  //           menuItems.items[i].menuId = result._id;
-  //         }
-
-  //         menusHelpers.postMenuItems(menuItems)
-  //           .then(function (data) {
-  //             console.log('\nMENU ITEMS', data);
-  //           });
-
-  //         done();
-  //       });
-  //   });
-
-  //   it('should be a function', function () {
-  //     expect(typeof vendorsHelpers.postVendor).toBe('function');
-  //   });
-
-  //   it('should retreive a vendor from the database', function (done) {
-  //     var getPromise = vendorsHelpers.getVendor(mockVendor);
-
-  //     getPromise
-  //       .then(function (result) {
-  //         expect(result.name).toBe("Willy's Chili");
-  //         done();
-  //       });
-  //   });
-
-  //   it('should return an error if the vendor does not exist', function (done) {
-  //     mockVendor.name = "Little Willy's Chili";
-  //     var getPromise = vendorsHelpers.getVendor(mockVendor);
-
-  //     getPromise
-  //       .then(function (result) {
-  //         expect(result.message).toBe('Vendor does not exist.');
-  //         done();
-  //       });
-  //   });
