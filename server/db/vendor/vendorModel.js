2a8a01e09b39ad27cf50422ced65e095d0ddb7db
Tom Bergen <thomasbergen15@gmail.com>
2016-01-27 10:20:46 -0800

    [refactor] update vendor schema

--- server/db/vendor/vendorModel.js
+++ server/db/vendor/vendorModel.js
@@ -4,20 +4,18 @@ var utils = require('../../config/utilities.js');
 var Schema = mongoose.Schema;
 
 var vendorSchema = new Schema({
-  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  salt: { type: String, required: true },
-  isVendor: { type: Boolean, default: true },
+  salt: { type: String },
   name: String,
   description: String,
-  cuisine: [String],
+  cuisine: String,
   imageUrl: String,
   location: String,
   menuItems: [{ type: Schema.Types.ObjectId, ref: 'MenuItem' }]
 });
 
 vendorSchema.pre('validate', utils.hashPassword);
-
 vendorSchema.methods.checkPassword = utils.comparePassword;
 
 module.exports = mongoose.model('Vendor', vendorSchema);
