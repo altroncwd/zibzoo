16335f1a785bfd95f3b7d4cd00d11a07e07e760e
Tom Bergen <thomasbergen15@gmail.com>
2016-01-27 13:31:05 -0800

    [feat] add signUp() and stub out signIn()

--- server/routes/customer/customerController.js
+++ server/routes/customer/customerController.js
@@ -1,77 +1,60 @@
-var Customer = require('../../db/customer/customerModel.js');
-var jwt = require('jwt-simple');
-var mongoose = require('mongoose');
-var env = require('env');
-var SWIPE_API_KEY = process.env.SWIPE_API_KEY || env.SWIPE_TEST_API_KEY;
-
-mongoose.Promise = require('bluebird');
+var customerHelpers = require('../../db/customer/customerHelpers.js');
+var utils = require('../../config/utilities.js');
 
 module.exports = {
 
-  signUp: function (req, res, next) {
-    var email = req.body.email;
-    var password = req.body.password;
+  signUp: function (req, res) {
+    var customer = {
+      email: req.body.email,
+      password: req.body.password
+    };
 
-    Customer.findOne({
-      email: email
-    })
-    .then(function (customer) {
-      if (customer) {
-        res.status(403).send({ error: 'Customer already exists' });
-        next(new Error('Customer already exists'));
-      }
-
-      var newCustomer = {
-        email: email,
-        password: password,
-      };
-
-      newCustomer = new Customer(newCustomer);
-      return newCustomer.save();
-    })
-    .then(function (customer) {
-      res.json({
-        id: customer._id,
-        email: customer.email,
-        isVendor: customer.isVendor,
-        token: jwt.encode(customer, 'secret')
+    customerHelpers.postCustomer(customer)
+      .then(function (result) {
+        utils.authSignUp(result, res);
+      })
+      .catch(function (error) {
+        return error;
       });
-    })
-    .catch(function (error) {
-      return error;
-    });
   },
 
-  signIn: function (req, res, next) {
-    var email = req.body.email;
-    var password = req.body.password;
+  signIn: function (req, res) {
+    var customer = {
+      email: req.body.email,
+      password: req.body.password
+    };
 
-    Customer.findOne({
-      email: email
-    })
-    .then(function (customer) {
-      if (!customer) {
-        res.status(401).send({ error: 'Customer does not exist' });
-        next(new Error('Customer does not exist'));
-      }
+    customerHelpers.getCustomer(customer)
+      .then()
+      .catch();
 
-      return customer.checkPassword(password)
-        .then(function (isMatch) {
-          if (isMatch) {
-            res.json({
-              id: customer._id,
-              email: customer.email,
-              isVendor: customer.isVendor,
-              token: jwt.encode(customer, 'secret')
-            });
-          } else {
-            res.status(401).send('Customername/password does not exist');
-            next(new Error('Customername/password does not exist'));
-          }
-        });
-    })
-    .catch(function (error) {
-      return error;
-    });
+    // Customer.findOne({
+    //   email: email
+    // })
+    //   .then(function (customer) {
+    //     if (!customer) {
+    //       res.status(401).send({ error: 'Customer does not exist' });
+    //       next(new Error('Customer does not exist'));
+    //     }
+
+    //     return customer.checkPassword(password)
+    //       .then(function (isMatch) {
+    //         if (isMatch) {
+    //           res.json({
+    //             id: customer._id,
+    //             email: customer.email,
+    //             isVendor: customer.isVendor,
+    //             token: jwt.encode(customer, 'secret')
+    //           });
+    //         } else {
+    //           res.status(401).send('Customername/password does not exist');
+    //           next(new Error('Customername/password does not exist'));
+    //         }
+    //       });
+    //   })
+    //   .catch(function (error) {
+    //     return error;
+    //   });
   }
+
 };
