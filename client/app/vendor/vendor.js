76c1a65f39cbbad2afcade3e801fe661b9b86d62
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-27 11:13:07 -0800

    [fix] update vendor controller for variable name change

--- client/app/vendor/vendor.js
+++ client/app/vendor/vendor.js
@@ -1,7 +1,7 @@
 angular.module('zibzoo.vendor', [])
   .controller('VendorController', ['$scope', '$stateParams', '$modal', 'vendor', 'Auth', function ($scope, $stateParams, $modal, vendor, Auth) {
     $scope.vendor = vendor.vendor;
-    $scope.items  = _.chunk($scope.vendor.menu, 2);
+    $scope.items  = _.chunk($scope.vendor.menuItems, 2);
 
     $scope.getVendor = function (params) {
       vendor.getVendors(params)
