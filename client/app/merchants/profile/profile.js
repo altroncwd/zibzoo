d2a4681d854a4bb076a002bcf2523723f1a289b5
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-27 14:41:28 -0800

    [feat] store first api call to localstorage for refresh persistence

--- client/app/merchants/profile/profile.js
+++ client/app/merchants/profile/profile.js
@@ -1,21 +1,16 @@
 angular.module('zibzoo.merchant', [])
   .controller('MerchantProfileController', ['$scope', 'vendor', '$stateParams', 'User', function ($scope, vendor, $stateParams, User) {
 
+    User.getFromLocal();
+
     $scope.img = 'http://rlv.zcache.com/i_love_food_trucks_square_sticker-r21025c827b5f4cb9823264e110552eeb_v9wf3_8byvr_324.jpg';
     $scope.cuisines = ['American', 'Burger', 'Fusion', 'Asian', 'Spicy'];
     $scope.vendor = User.data;
 
     $scope.merchantId = $stateParams.merchantId;
 
-    $scope.remove = function (index) {
-      $scope.cuisine.splice(index, 1);
-    };
-
-    $scope.addCuisine = function (food) {
-      $scope.cuisine.push(food);
-    };
-
     $scope.updateVendor = function (updatedVendor) {
+      User.setNewToLocal();
       vendor.updateVendor(updatedVendor)
         .then(function (data) {
           $scope.updateStatus = data.status;
