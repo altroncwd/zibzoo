64ceac8cee7a958f88eea22b9a17f3915946ce8a
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-26 19:55:37 -0800

    [test] update menu controller spec

--- spec/client/merchantMenuControl.spec.js
+++ spec/client/merchantMenuControl.spec.js
@@ -3,7 +3,7 @@ describe('MerchantMenuController', function () {
   var $rootScope;
   var createController;
   var $httpBackend;
-  var vendor;
+  var user;
   var menu;
 
   beforeEach(module('zibzoo'));
@@ -11,7 +11,7 @@ describe('MerchantMenuController', function () {
 
     $rootScope = $injector.get('$rootScope');
     $httpBackend = $injector.get('$httpBackend');
-    vendor = $injector.get('vendor');
+    user = $injector.get('User');
     menu = $injector.get('menu');
 
     $scope = $rootScope.$new();
@@ -21,13 +21,12 @@ describe('MerchantMenuController', function () {
     createController = function () {
       return $controller('MerchantMenuController', {
         $scope: $scope,
-        vendor: vendor,
+        user: user,
         menu: menu
       });
     };
 
     var fakeData = [{}, {}, {}, {}];
-    $httpBackend.whenGET('api/vendors').respond(fakeData);
     $httpBackend.whenGET('app/landing/landing.html').respond(fakeData);
     $httpBackend.whenPOST('api/menu').respond(201);
     $httpBackend.whenDELETE('api/menu').respond(204);
@@ -41,6 +40,24 @@ describe('MerchantMenuController', function () {
     $httpBackend.verifyNoOutstandingRequest();
   });
 
+  describe('$scope.vendor', function () {
+    it('should be a property on the scope object', function () {
+      expect($scope.vendor).toBeDefined();
+    });
+    it('should be assigned to User.data on controller load', function () {
+      expect($scope.vendor).toBe(user.data);
+    });
+    it('should not be changed by any other function in the same scope',
+      function () {
+        $scope.toggle();
+        $scope.deleteMenuItem(0);
+        $scope.saveMenuItem({});
+        $scope.clearItem();
+        $httpBackend.flush();
+        expect($scope.vendor).toBe(user.data);
+      });
+  });
+
   describe('$scope.menu', function () {
     it('should be a property on the $scope', function () {
       expect($scope.menu).toBeDefined();
@@ -48,10 +65,19 @@ describe('MerchantMenuController', function () {
     it('should be assigned to the menu factory', function () {
       expect($scope.menu).toBe(menu);
     });
+    it('should not be changed by any other function in the same scope',
+      function () {
+        $scope.toggle();
+        $scope.deleteMenuItem(0);
+        $scope.saveMenuItem({});
+        $scope.clearItem();
+        $httpBackend.flush();
+        expect($scope.menu).toBe(menu);
+      });
   });
 
   describe('$scope.menuItem', function () {
-    it('should be a property on the$scope object', function () {
+    it('should be a property on the $scope object', function () {
       expect($scope.menuItem).toBeDefined();
     });
     it('should have a blank Menu Item saved to it on Page load',
@@ -103,15 +129,26 @@ describe('MerchantMenuController', function () {
     });
   });
 
+  describe('$scope.toggle', function () {
+    it('should be a property on the $scope object', function () {
+      expect($scope.toggle).toBeDefined();
+    });
+    it('should be a function', function () {
+      expect(typeof $scope.toggle).toBe('function');
+    });
+  });
+
   describe('$scope.deleteMenuItem', function () {
     it('should be a function on the scope object', function () {
       expect(typeof $scope.deleteMenuItem).toBe('function');
     });
     it('should make a delete request by calling $scope.menu.deleteMenuItem', function () {
+      spyOn($scope.menu, 'remove').and.callThrough();
       spyOn($scope.menu, 'deleteMenuItem').and.callThrough();
       $scope.deleteMenuItem(5);
       $httpBackend.flush();
       expect($scope.deleteStatus).toEqual(204);
+      expect($scope.menu.remove).toHaveBeenCalled();
       expect($scope.menu.deleteMenuItem).toHaveBeenCalled();
     });
     it('should throw an error when an error code is recieved', function () {
@@ -127,9 +164,13 @@ describe('MerchantMenuController', function () {
       expect(typeof $scope.saveMenuItem).toBe('function');
     });
     it('should make a post request calling $scope.menu.saveMenutItem', function () {
+      spyOn(angular, 'extend').and.callThrough();
+      spyOn($scope, 'clearItem').and.callThrough();
       spyOn($scope.menu, 'saveMenuItem').and.callThrough();
       $scope.saveMenuItem({ name: 'fajita' });
       $httpBackend.flush();
+      expect($scope.clearItem).toHaveBeenCalled();
+      expect(angular.extend).toHaveBeenCalled();
       expect($scope.menu.saveMenuItem).toHaveBeenCalled();
       expect($scope.saveStatus).toEqual(201);
     });
