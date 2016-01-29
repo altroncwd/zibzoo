8a5df5600dc2cca8e53c34148c4ba34f069f5599
Ian Bari <ian.bari@att.net>
2016-01-28 18:47:05 -0800

    [refactor]: client auth should make separate api calls for customers/vendors

--- server/routes/vendor/vendorRoutes.js
+++ server/routes/vendor/vendorRoutes.js
@@ -3,7 +3,7 @@ var vendorController = require('./vendorController.js');
 module.exports = function (app) {
 
   app.post('/signup', vendorController.signUp);
-  app.get('/signin', vendorController.signIn);
+  app.post('/signin', vendorController.signIn);
   app.get('/', vendorController.retrieveVendors);
   app.put('/', vendorController.updateVendor);
 
