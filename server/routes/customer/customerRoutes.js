81f9c7483bbd943350a3e7b8bd69d4dfc592a5cf
Tom Bergen <thomasbergen15@gmail.com>
2016-01-29 11:22:32 -0800

    [feat] add customer/checkout route stub

--- server/routes/customer/customerRoutes.js
+++ server/routes/customer/customerRoutes.js
@@ -5,5 +5,8 @@ module.exports = function (app) {
   app.post('/signup', customerController.signUp);
   app.post('/signin', customerController.signIn);
   app.put('/', customerController.updateCustomer);
+  app.post('/checkout', function () {
+    // TODO: add checkout function
+  });
 
 };
