2ed42105ea5de3ffd3f8f13188b17286652f2e5f
Christopher Decker <christopher.w.decker@gmail.com>
2016-01-28 17:09:50 -0800

    [fix] minor spacing for linting

--- client/app/vendor/vendor.js
+++ client/app/vendor/vendor.js
@@ -1,6 +1,6 @@
 angular.module('zibzoo.vendor', [])
   .controller('VendorController', ['$scope', '$stateParams', '$modal', 'vendor', 'Auth', function ($scope, $stateParams, $modal, vendor, Auth) {
-    //$scope.vendor = vendor.data;
+    // $scope.vendor = vendor.data;
     $scope.vendor = function (data) {
       for (var i = 0; i < data.length; i++) {
         if (data[i].id === $stateParams.vendorId) {
