c3bc498a1db14c197b7464360e9958bc692d132f
Christopher Decker <christopher.w.decker@gmail.com>
2016-02-02 12:40:17 -0800

    [fix] update for linting fixes

--- client/app/components/map/geolocation-factory.js
+++ client/app/components/map/geolocation-factory.js
@@ -1,5 +1,5 @@
 angular.module('zibzoo.geolocation.factory', [])
-  .factory('geolocation', ['$rootScope', function ($rootScope) { 
+  .factory('geolocation', ['$rootScope', function ($rootScope) {
     var geolocation = {};
 
     geolocation.data = {
@@ -19,7 +19,7 @@ angular.module('zibzoo.geolocation.factory', [])
       var geocoder = geocoder = new google.maps.Geocoder();
 
       geocoder.geocode({ 'latLng': latlng }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
+        if (status === google.maps.GeocoderStatus.OK) {
           if (results[1]) {
             geolocation.data.address = results[1].address_components[0].long_name;
             $rootScope.$apply();
@@ -32,25 +32,25 @@ angular.module('zibzoo.geolocation.factory', [])
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(geolocation.setLocation, geolocation.setError);
       } else {
-        geolocation.data.error = "Geolocation is not supported by this browser.";
+        geolocation.data.error = 'Geolocation is not supported by this browser.';
         $rootScope.$apply();
       }
     };
-   
+
     geolocation.setError = function (error) {
       switch (error.code) {
-        case error.PERMISSION_DENIED:
-          geolocation.data.error = "User denied the request for Geolocation."
-          break;
-        case error.POSITION_UNAVAILABLE:
-          geolocation.data.error = "Location information is unavailable."
-          break;
-        case error.TIMEOUT:
-          geolocation.data.error = "The request to get user location timed out."
-          break;
-        case error.UNKNOWN_ERROR:
-          geolocation.data.error = "An unknown error occurred."
-          break;
+      case error.PERMISSION_DENIED:
+        geolocation.data.error = 'User denied the request for Geolocation.';
+        break;
+      case error.POSITION_UNAVAILABLE:
+        geolocation.data.error = 'Location information is unavailable.';
+        break;
+      case error.TIMEOUT:
+        geolocation.data.error = 'The request to get user location timed out.';
+        break;
+      case error.UNKNOWN_ERROR:
+        geolocation.data.error = 'An unknown error occurred.';
+        break;
       }
 
       $rootScope.$apply();
