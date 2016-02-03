744248e0385167adbac112d69412bb49d2cde2dc
Christopher Decker <christopher.w.decker@gmail.com>
2016-02-02 16:38:22 -0800

    [feature] remove console statments from socket

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -5,12 +5,7 @@ module.exports = function (server) {
   var io = sockets(server);
 
   io.on('connect', function (socket) {
-    console.log('Hyperdrive socket now connected');
-
     socket.on('order finished', function (finsihedOrder) {
-      console.log('User email:', finsihedOrder.customerInfo.email, ': SERVER SIDE');
-
-      // Uncomment the line below for emails to work
       mail.sendMail(finsihedOrder);
     });
 
