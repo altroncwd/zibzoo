83b7a0299c0b7678af8a4d937a31b6d345d0f71f
Ian Bari <ian.bari@att.net>
2016-02-03 14:56:22 -0800

    [feat]: add google maps autocomplete

--- client/app/landing/landing.js
+++ client/app/landing/landing.js
@@ -1,7 +1,16 @@
 angular.module('zibzoo.landing', [])
-  .controller('LandingController', ['$rootScope', '$scope', 'vendor', 'geolocation', function ($rootScope, $scope, vendor, geolocation) {
-    geolocation.getLocation();
-
-    $scope.geolocation = geolocation.data;
+  .controller('LandingController', ['$scope', '$state', 'vendor', 'location', function ($scope, $state, vendor, location) {
+    $scope.location = location.data;
     $scope.vendors = vendor.tempData;
+
+    $scope.findVendors = function () {
+      if ($scope.selectedPlace) {
+        var latitude = $scope.selectedPlace.geometry.location.lat();
+        var longitude = $scope.selectedPlace.geometry.location.lng();
+        
+        $state.go('vendors', { latlng: latitude + ',' + longitude });
+      }
+    };
+
+    location.getCurrentLocation();
   }]);
